268280dbcdacf9b855ca2bf286bdb7313d8230cf
Shyam Agarwal <shyam.agarwal1v@gmail.com>
2026-07-12 22:02:20 +0530

    add logout feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
 
   def new
     @user= User.new
@@ -21,6 +22,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to AlphaBlog! User was successfully created"
       UserMailer.welcome_email(@user).deliver_later
       redirect_to articles_path
@@ -42,6 +44,10 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
